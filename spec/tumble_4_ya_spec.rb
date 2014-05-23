1d827c959363fca20fa57e8fefa1312677fe3f72
michaeltomko <mike@tomkobombco.com>
2014-05-23 15:29:46 -0500

    Added tests for .score_items method.

--- spec/tumble_4_ya_spec.rb
+++ spec/tumble_4_ya_spec.rb
@@ -15,6 +15,16 @@ describe Tumble4Ya do
     end  
   end
 
+  def try_to_score
+    @scored_array = Tumble4Ya::Tumbler.new(drinks).score_items do |drink|
+      [(drink.length > 4 ? 1 : 0),
+        (drink.match(/^j/) ? 1 : 0),
+        (alcohol.include?(drink) ? 1 : 0),
+        1
+      ]
+    end
+  end
+
   it 'should roulette wheel sort an array' do
     try_to_tumble
     expect(@tumbled_array).to be_a Array
@@ -27,6 +37,18 @@ describe Tumble4Ya do
 
   it 'should handle cases where the array of bits is all zeros' do
     expect(drinks.tumble{|drink| all_zeros }).to be_a Array
+  end
+
+  it 'should return scored items when .score_items is called' do
+    try_to_score
+    expect(@scored_array).to be_a Array
+    expect(drinks).to include(@scored_array.first[:item])
+  end
+
+  it 'should output the score as a string and the string should only contain 1s and 0s' do
+    try_to_score
+    expect(@scored_array.first[:score]).to be_a String
+    expect(@scored_array.first[:score]).to match(/[01]+/)
   end
   
 end
